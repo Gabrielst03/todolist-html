121b16c99a1778cf4a4efcd488774493dbbf8111
Gabriel Santana <santanag311@gmail.com>
2024-01-15 11:41:35 -0300

    Subindo projeto

---
+++ script.js
@@ -0,0 +1,38 @@
+function addTask() {
+    var taskInput = document.getElementById('newTask');
+    var taskList = document.getElementById('taskList');
+
+    if(taskInput.value.trim() != '') {
+        var newTask = document.createElement('li')
+        newTask.className = "group flex items-center justify-between p-2 border-b w-[400px]"
+        newTask.innerHTML = "<span>" + taskInput.value.trim() + "</span>" + "<button class='text-red-500 hidden group-hover:block' onclick='removeTask(this)'>Excluir</button>"
+
+        taskList.appendChild(newTask)
+        taskInput.value = ''
+        Toastify({
+            text: "Tarefa criada com sucesso!",
+            style: {
+                background: 'green'
+            }
+        }).showToast()
+    } else {
+        Toastify({
+            text: "Erro: Preencha seus dados!",
+            style: {
+                background: 'red'
+            }
+        }).showToast()
+    }
+}
+
+function removeTask(button) {
+    var taskItem = button.parentNode;
+    taskItem.parentNode.removeChild(taskItem)
+
+    Toastify({
+        text: "Tarefa excluída com sucesso!",
+        style: {
+            background: 'green'
+        }
+    }).showToast()
+}
